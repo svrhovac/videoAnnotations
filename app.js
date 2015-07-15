6975b8776fc6b85a20fc9893bb3862078b8a06ff
Marko Ivanovic <kormang@hotmail.rs>
2015-07-15 09:11:20 +0200

    Getting list of videos is done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var routes = require('./routes/index');
 
@@ -18,6 +19,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'html')));
@@ -37,11 +39,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.sendStatus(err.status || 500);
   });
 }
 
