9f195242f9a470854a122dd4069ec0a57a4d0e6a
milan <milanboji@gmail.com>
2015-07-21 16:10:19 +0200

    statistics client done! Vesion 1.2

--- public/js/controllers/statisticController.js
+++ public/js/controllers/statisticController.js
@@ -1,21 +1,25 @@
 spa.controller("statisticController", function($scope, $routeParams, videoService, statisticService){
-	//$scope.sVideos = videoService.getVidList();
+	$scope.videosStat = [];
+	$scope.totalViews = 0;
+	
 	videoService.getVideos().success(function(data){
 		$scope.sVideos = data;
+		for (var i=0; i< $scope.sVideos.length; i++){
+			
+			statisticService.getStat($scope.sVideos[i]._id).success(function(data){
+				$scope.videosStat.push(data);
+				$scope.totalViews += data.countView;
+				}).
+				error(function(data){
+					console.log("Connection with server interrupted!!!");
+				});
+		}
 	}).
 	error(function(data){
 		console.log("Connection with server interrupted!!!");
 	});
-	
-    $scope.getStatistics = function(sVid){
-		//$scope.stat = statisticService.getStat(sVid._id);
-		//console.log(sVid);
-		statisticService.getStat(sVid._id).success(function(data){
-		$scope.stat = data;
-	}).
-	error(function(data){
-		console.log("Connection with server interrupted!!!");
-	});
-		
+
+	$scope.getStatistics = function(statId){
+		$scope.stat = $scope.videosStat[statId];	
 	};
 });
