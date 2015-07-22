9e9e383a1f243d6fa6f939ae9a976a82eac8e90d
EditMilica <marjanac.milica@gmail.com>
2015-07-22 10:47:30 +0200

    result conflict issues fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 
-var spa=angular.module("app", ["ngRoute","ngAnimate"]);
+var spa=angular.module("app", ["ngRoute"]);
             
 spa.config(['$routeProvider',
 function($routeProvider) {
@@ -13,11 +13,7 @@ $routeProvider.
         templateUrl: 'views/videoView.html',
         controller: 'videoController'
     }).
-<<<<<<< HEAD
-    when('statistic', {
-=======
 	when('/statistic',{
->>>>>>> 71c0fcf9dfa6b077b49d2d726ac7204b7d130daa
         templateUrl: 'views/statisticView.html',
         controller: 'statisticController'
     }).
