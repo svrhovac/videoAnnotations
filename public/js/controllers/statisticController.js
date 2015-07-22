0767ba42b44deca1dc2291b5be3b91165da97924
milan <milanboji@gmail.com>
2015-07-22 13:15:13 +0200

    statistcis view with sort

--- public/js/controllers/statisticController.js
+++ public/js/controllers/statisticController.js
@@ -1,25 +1,43 @@
 spa.controller("statisticController", function($scope, $routeParams, videoService, statisticService){
-	$scope.videosStat = [];
+	//$scope.videosStat = [];
+	$scope.tableData = [];
 	$scope.totalViews = 0;
+	//$scope.i = 0;
 	
-	videoService.getVideos().success(function(data){
-		$scope.sVideos = data;
-		for (var i=0; i< $scope.sVideos.length; i++){
+	videoService.getVideos().success(function(videoData){
+		//$scope.sVideos = videoData;
+		for (var i = 0; i< videoData.length; i++){
 			
-			statisticService.getStat($scope.sVideos[i]._id).success(function(data){
-				$scope.videosStat.push(data);
-				$scope.totalViews += data.countView;
-				}).
-				error(function(data){
-					console.log("Connection with server interrupted!!!");
-				});
+			statisticService.getStat(videoData[i],function(anoData, video){
+				$scope.totalViews += anoData.countView;
+				var obj = {
+					"title":video.title,
+					"author":video.author,
+					"duration":video.duration,
+					"countView":anoData.countView,
+					"lastViewDate":anoData.lastViewDate, 
+					"numberOfAnnotation":anoData.numberOfAnnotation
+				}
+				$scope.tableData.push(obj);
+				
+			});
 		}
 	}).
 	error(function(data){
 		console.log("Connection with server interrupted!!!");
 	});
 
+	//TODO ne prikazuje odgovarajuci
 	$scope.getStatistics = function(statId){
-		$scope.stat = $scope.videosStat[statId];	
+		$scope.stat = statId;
 	};
+	
+	//Sortiranje
+	$scope.predicate = 'title';
+	$scope.reverse = true;
+	$scope.order = function(predicate) {
+		$scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+		$scope.predicate = predicate;
+	};
+	
 });
